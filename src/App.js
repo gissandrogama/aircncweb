fcdb442d402b76aaca7f5b0a9e4756ae0a218be0
gissandrogama <gissandrogama@gmail.com>
2019-10-17 00:16:11 -0300

    conponetne de login finalizado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Routes from './routes'
+
 import './App.css';
 
 import logo from './assets/logo.svg'
@@ -8,19 +10,7 @@ function App() {
     <div className="container">
       <img src={logo} alt="AirCnC" />
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
-        </p>
-        <form>
-          <label htmlFor="email">E-mail *</label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Seu melhor e-mail"
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
